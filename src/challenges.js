dccf8b08cfc70831abb1fa39856e4b887bc1de52
Kaike Yudi <kaikeyudi@outlook.com>
2021-12-02 15:54:18 -0300

    feat: implementa função splitSentence

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  const splitedSentence = sentence.split(' ');
+  return splitedSentence;
 }
 
 // Desafio 4
